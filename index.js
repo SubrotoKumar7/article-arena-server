87c63c6c919c01d4ad322360edca24cc2a6320cd
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-09 20:07:08 +0600

    user get and post api added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 5000;
+const saltRounds = 10;
 
 // middleware
 app.use(express.json());
@@ -28,6 +30,24 @@ const client = new MongoClient(uri, {
     try {
         await client.connect();
 
+        const database = client.db('article_arena');
+        const usersCollections = database.collection('users');
+
+        // ? users related api
+        app.get('/users', async(req, res)=> {
+            const query = {};
+            const cursor = usersCollections.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async(req, res)=> {
+            const userData = req.body;
+            const salted = await bcrypt.hash(userData.password, saltRounds);
+            userData.password = salted;
+            const result = await usersCollections.insertOne(userData);
+            res.send(result);
+        })
 
 
         await client.db("admin").command({ ping: 1 });
